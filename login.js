787ad726b6ea8670ad0e9a66a6801ab150e3b1b4
Vlad <vgrechannik@gmail.com>
2019-11-06 00:02:01 +0300

    This gonna ruin my whole life

--- login.js
+++ login.js
@@ -1,7 +1,44 @@
+let name = document.getElementById("name");
+name.addEventListener("input", function (event) {
+    if (!name.validity.patternMismatch) {
+        name.setCustomValidity("");
+    } else {
+        name.setCustomValidity("Ваше имя должно начинаться с большой буквы и состоять из букв английского или русского языка и содержать в себе только буквы");
+    }
+});
+let secondname = document.getElementById("secondname");
+secondname.addEventListener("input", function (event) {
+    if (!secondname.validity.patternMismatch) {
+        secondname.setCustomValidity("");
+    } else {
+        secondname.setCustomValidity("Ваша фамилия должна начинаться с большой буквы и состоять из букв английского или русского языка и содержать в себе только буквы");
+    }
+});
+let fathername = document.getElementById("fathername");
+fathername.addEventListener("input", function (event) {
+    if (!fathername.validity.patternMismatch) {
+        fathername.setCustomValidity("");
+    } else {
+        fathername.setCustomValidity("Ваше отчество должно начинаться с большой буквы и состоять из букв английского или русского языка и содержать в себе только буквы");
+    }
+});
+let email = document.getElementById("email");
+email.addEventListener("input", function (event) {
+    if (!email.validity.patternMismatch) {
+        email.setCustomValidity("");
+    } else {
+        email.setCustomValidity("Вы должны закончить адрес почты написанием доменного имени. Пример:...@mail.ru");
+    }
+});
+let password = document.getElementById("password");
+password.addEventListener("input", function (event) {
+    if (!password.validity.patternMismatch) {
+        password.setCustomValidity("");
+    } else {
+        password.setCustomValidity("Пароль должен содержать как минимум цифры, символы нижнего и верхнего регистра.");
+    }
+});
 document.getElementById("form").onsubmit = function (evt) {
-    localStorage.clear();
-    evt.preventDefault();
-    console.log(evt);
     let object = {};
     for (let i = 0; i < 7; i++) {
         object[evt.target[i].name] = evt.target[i].value;
